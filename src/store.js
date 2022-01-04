02d8389ca170081647543642d7e7303631b5f555
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-01-04 20:09:19 +0300

    currentUser added

--- src/store.js
+++ src/store.js
@@ -3,3 +3,4 @@ import { writable, readable } from 'svelte/store'
 export let searchRequest = writable();
 export let selectedTicketId = writable();
 export let userId = writable();
+export let currentUser = writable();
