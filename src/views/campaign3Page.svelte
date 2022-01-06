99ffa51b7ff3626fd56e130fb247f13a8eb41b17
Sergen Berke Altan <sergen.ege@hotmail.com>
2022-01-06 17:48:36 +0300

    test

--- src/views/campaign3Page.svelte
+++ src/views/campaign3Page.svelte
@@ -5,20 +5,14 @@
 
 <main>
     <div class="section d-flex justify-content-center align-items-center ">
-
     <Navbar />
-
         <div class="blog">
-            <h2>THY ile İndirim Fırsatı</h2>
-    
+            <h2>THY ile İndirim Fırsatı</h2>   
             <p class="paragraph">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptates officia tempora doloremque hic consequuntur, veniam voluptas quidem at odit iure nam voluptate quae quis? Commodi illum tempora placeat, ex molestias voluptatum ut consequuntur laborum ad, vitae soluta reiciendis a non veniam distinctio magnam eos quis ducimus optio officiis. Repellat eum quaerat quo, earum totam officia, commodi, animi consequatur possimus nam quas sequi omnis tempore beatae consequuntur aliquam enim dolorum voluptatum iusto sunt necessitatibus asperiores. Laboriosam asperiores quam natus sequi illo sunt! Sequi, ad. Autem, aperiam perspiciatis! Voluptate doloremque, enim quibusdam omnis amet ad velit dolorum vero labore esse libero?
             </p>
-    
-    
         </div>
     </div>
-
 </main>
 
 <style>
@@ -50,6 +44,4 @@ h2{
     font-size: 2.0rem;
     color: #FE0000;
 }
-
-
 </style>
