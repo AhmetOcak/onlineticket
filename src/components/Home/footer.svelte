1dc63536cb430731d7a85bd32245193d6f270a7c
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-01-25 15:42:33 +0300

    year uptaded

--- src/components/Home/footer.svelte
+++ src/components/Home/footer.svelte
@@ -37,7 +37,7 @@
                 </div>
                 <div class="row mt-5">
                     <div class="col">
-                        <p class=""><small class="text-white-50">© 2021. All Rights Reserved.</small></p>
+                        <p class=""><small class="text-white-50">© 2022. All Rights Reserved.</small></p>
                     </div>
                 </div>
             </div>
