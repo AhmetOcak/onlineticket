508a7608b2fefe4b12199dda550f106e44b1433d
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-01-26 16:26:31 +0300

    reorganized

--- src/components/Bill/billCard.svelte
+++ src/components/Bill/billCard.svelte
@@ -13,7 +13,7 @@
         <div class="card-header fs-4">{companyName}</div>
         <div class="card-body">
             <h5 class="card-title">Sayın {userName}</h5>
-            <p class="card-text">BİLET ID : {ticketID}</p>
+            <p class="card-text">Fatura ID : {ticketID}</p>
             <p class="card-text">Fatura Tutarı : {billPrice} TL</p>
             <p class="card-text">İşlemin Yapıldığı Tarih : {date}</p>
             <p class="card-text">İşlem Kanalının Adı : {purchasePlace}</p>
