4d5689be8cbc9d60db3f971b001cc4c1b8c1a6b2
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-01-29 14:54:26 +0300

    SvelteToast added

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,16 @@
 <script>
 	import Router from 'svelte-spa-router';
 	import routes from './router/routes.js';
+	import { SvelteToast } from '@zerodevx/svelte-toast'
+
+	const app = new SvelteToast({
+		target: document.body,
+		props: {
+			options: {
+				duration: 3000,
+			}
+		}
+	});
 </script>
 
 <Router {routes} />
