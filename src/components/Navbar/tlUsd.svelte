f74d6ff56428c051504b3d2e480e3d9216ded3af
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-01-31 19:06:08 +0300

    currency end

--- src/components/Navbar/tlUsd.svelte
+++ src/components/Navbar/tlUsd.svelte
@@ -5,25 +5,52 @@
 
     let currencyUrl = 'https://onlineticketbackendapi.azure-api.net/v1/api/Currency_Units';
     let currencies = [];
+    let defaultCurrency = "TL";
 
     onMount(async () => {
+        if(getCookie("currency") == null) {
+            document.cookie = `currency=${defaultCurrency}`;
+        }
         try{
             currencies = await (await axios.get(currencyUrl)).data[0].currencyUnit;
+            console.log(currencies);
         } catch(e) {
             console.log(e);
         }
     });
 
+    function getCookie(cookieName) {
+        let cookie = {};
+        document.cookie.split(';').forEach(function(el) {
+            let [key,value] = el.split('=');
+            cookie[key.trim()] = value;
+        })
+        return cookie[cookieName];
+    }
 
 </script>
 
 <main>
-    <select class="form-select form-select-sm bg-dark" aria-label=".form-select-sm example">
-        {#each currencies as currency}
-        <option selected>{currency}</option>
+    <!-- svelte-ignore missing-declaration -->
+    <select id="select" class="form-select form-select-sm bg-dark" aria-label=".form-select-sm example" on:click={() => {
+        let currency = select.value;
+        document.cookie = `currency=${currency}`;
+    }}>
+        {#if getCookie("currency") == "EURO"}
+        <option selected>{currencies[0]}</option>
+        <option>{currencies[1]}</option>
+        <option>{currencies[2]}</option>
+        {:else if getCookie("currency") == "USD"}
+        <option>{currencies[0]}</option>
+        <option selected>{currencies[1]}</option>
+        <option>{currencies[2]}</option>
+        {:else if getCookie("currency") == "TL"}
+        <option>{currencies[0]}</option>
+        <option>{currencies[1]}</option>
+        <option selected>{currencies[2]}</option>
         {:else}
-        <option value="2">Loading</option>
-        {/each}
+        <option>{defaultCurrency}</option>
+        {/if}
     </select>
 </main>
 
