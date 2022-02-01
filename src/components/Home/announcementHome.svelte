34236682dd406abdcbafb6e07d62cbe9568fc70f
Sergen Berke Altan <sergen.ege@hotmail.com>
2022-02-01 22:34:03 +0300

    AnnouncementHome texts changed

--- src/components/Home/announcementHome.svelte
+++ src/components/Home/announcementHome.svelte
@@ -9,10 +9,10 @@
           Duyurular
         </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item"> <a href="/announcementPage" use:link>Lorem ipsum dolor sit amet.</a> 
+          <li class="list-group-item"> <a href="/announcementPage" use:link>Turkish Airlines Lounge Hizmetlerindeki Değişiklikler</a> 
           </li>
-          <li class="list-group-item"> <a href="/announcementPage" use:link>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, itaque.</a> </li>
-          <li class="list-group-item"> <a href="/announcementPage" use:link>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</a> </li>
+          <li class="list-group-item"> <a href="/announcementPage" use:link>Güncel Seyahat Kuralları</a> </li>
+          <li class="list-group-item"> <a href="/announcementPage" use:link>65 yaş ve Üstü Yolcularımızın Dikkatine</a> </li>
           <li class="text-primary list-group-item"> <a href="/announcementPage" use:link>Tümü</a></li>
 
         </ul>
